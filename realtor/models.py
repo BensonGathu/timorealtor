b205f6b816d3f531e93bea9a45959ff5f39f73a9
Benson <bensongathu23@gmail.com>
2022-02-28 16:52:11 +0300

    dep

--- realtor/models.py
+++ realtor/models.py
@@ -124,5 +124,19 @@ class Land(models.Model):
     def __str__(self):
         return self.description
 
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    message = models.CharField(max_length=1200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+ 
+    def __str__(self):
+        return self.message
+ 
+    class Meta:
+        ordering = ('timestamp',)
+ 
     
 
